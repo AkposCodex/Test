6cc16f77083200c3ebe3ea5f4afe27a41c11feb0
AkposCodex <litle1akp@gmail.com>
2024-12-07 15:56:24 +0100

    .

--- src/components/AgentSignup.vue
+++ src/components/AgentSignup.vue
@@ -1,7 +1,6 @@
 <template>
   <section
-    class="flex flex-wrap gap-10 justify-between items-center text-left mt-28 max-w-full w-[1392px] max-md:mt-10"
-  >
+    class="flex flex-wrap gap-10 justify-between items-center text-left mt-28 max-w-full w-[1392px] max-md:mt-10">
     <div class="flex flex-col min-w-[240px] w-[600px] max-md:max-w-full">
       <div class="flex flex-col w-full text-black max-md:max-w-full">
         <h2 class="text-4xl font-bold max-md:max-w-full">
@@ -12,55 +11,36 @@
           the event.
         </p>
       </div>
-      <form class="flex flex-col mt-6 max-w-full text-lg w-[690px]">
-        <div
-          class="flex flex-col w-full font-light text-black max-md:max-w-full"
-        >
+      <form class="flex flex-col mt-6 max-w-full text-lg w-[690px]" action="https://formspree.io/f/xdkojked"
+        method="POST">
+        <div class="flex flex-col w-full font-light text-black max-md:max-w-full">
           <div class="mb-5">
             <label for="name" class="sr-only">Name</label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Name"
-              class="gap-2.5 self-stretch p-2.5 w-full whitespace-nowrap rounded border border-solid border-zinc-400 max-md:max-w-full"
-            />
+            <input type="text" id="name" placeholder="Name"
+              class="gap-2.5 self-stretch p-2.5 w-full whitespace-nowrap rounded border border-solid border-zinc-400 max-md:max-w-full" />
           </div>
           <div class="mb-5">
             <label for="phone" class="sr-only">Phone number</label>
-            <input
-              type="tel"
-              id="phone"
-              placeholder="Phone number"
-              class="gap-2.5 self-stretch p-2.5 w-full rounded border border-solid border-zinc-400 max-md:max-w-full"
-            />
+            <input type="tel" id="phone" placeholder="Phone number"
+              class="gap-2.5 self-stretch p-2.5 w-full rounded border border-solid border-zinc-400 max-md:max-w-full" />
           </div>
           <div class="mb-5">
             <label for="email" class="sr-only">Email address</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Email address(optional)"
-              class="gap-2.5 self-stretch p-2.5 w-full rounded border border-solid border-zinc-400 max-md:max-w-full"
-            />
+            <input type="email" id="email" placeholder="Email address(optional)"
+              class="gap-2.5 self-stretch p-2.5 w-full rounded border border-solid border-zinc-400 max-md:max-w-full" />
           </div>
         </div>
-        <button
-          type="submit"
-          class="gap-2.5 self-stretch p-2.5 mt-6 max-w-full font-medium text-white bg-cyan-500 rounded-xl w-[221px]"
-        >
+        <button type="submit"
+          class="gap-2.5 self-stretch p-2.5 mt-6 max-w-full font-medium text-white bg-cyan-500 rounded-xl w-[221px]">
           Sign up
         </button>
       </form>
     </div>
     <div
-      class="flex overflow-hidden flex-col justify-center py-px min-w-[240px] md:w-[300px] lg:block hidden max-lg:w-[400px]"
-    >
-      <img
-        loading="lazy"
+      class="flex overflow-hidden flex-col justify-center py-px min-w-[240px] md:w-[300px] lg:block hidden max-lg:w-[400px]">
+      <img loading="lazy"
         src="https://cdn.builder.io/api/v1/image/assets/c49e2462ddbf40d0944f306255d5e3e6/e82c2ebe0c20608c16409085e0280e5de4ead642dba5a1d581c314aef32f9b6c?apiKey=c49e2462ddbf40d0944f306255d5e3e6&"
-        alt="Agent activation event illustration"
-        class="object-contain w-full aspect-[0.7]"
-      />
+        alt="Agent activation event illustration" class="object-contain w-full aspect-[0.7]" />
     </div>
   </section>
 </template>
